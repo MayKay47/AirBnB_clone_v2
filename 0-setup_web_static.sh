f6845f5398ae93846c97dde699d1cdb38aa78646
Mayen Kalu <kalumayen@gmail.com>
2023-05-12 15:11:53 +0100

    update script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,28 +2,25 @@
 # sets up the web servers for the deployment of web_static
 
 # update the package lists
-sudo apt-get -y update
+apt-get -y update
 
 # install nginx
-sudo apt-get -y install nginx
+apt-get -y install nginx
 
 # make directories for the deployment using -p to create all parent directories if not existing
-sudo mkdir -p /data/web_static/releases/test 
-sudo mkdir -p /data/web_static/shared
-
-# give ownership of the /data/ directory to the ubuntu user 
-sudo chown -R ubuntu /data/
-
-# give group ownership of the /data/ directory to the ubuntu user
-sudo chgrp -R ubuntu /data/
+mkdir -p /data/web_static/releases/test 
+mkdir -p /data/web_static/shared
 
 # create a test HTML file
-echo "HBNB Static Testing" > /data/web_static/releases/test/index.html
+echo "HBNB Static Testing" >> /data/web_static/releases/test/index.html
 
 # create a symbolic link to the test HTML file
 #---'ln -s' creates a symbolic link
 #---'f' forces the symbolic link to be created if the target already exists, hence overwriting whenever target file is updated
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# give ownership of the /data/ directory to the ubuntu user 
+chown -R ubuntu:ubuntu /data/ 
 
 printf %s "server {
     listen 80 default_server;
